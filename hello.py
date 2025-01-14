cda731cda510040acbe9b39e20c2f5645d4a3756
Rayane Guessous <rayaneguessous@MacBook-Pro-7.local>
2025-01-14 17:40:26 -0500

    m

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+print(math.pi*rayon*rayon)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Rayane Guessous")
